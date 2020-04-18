07b03565f536c6d7d06267b6112483c1776797c2
Yuka <54751741+Yuka030@users.noreply.github.com>
2020-04-17 20:08:43 -0700

    Complete assignment2

---
+++ Assignment2/AutoLayoutIB/AutoLayoutIB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutIB
+//
+//  Created by Yuka Ishiwata on 2020-04-17.
+//  Copyright © 2020 Yuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
